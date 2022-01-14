5b97876466cf465d62d88e6c64e89e60f32618cd
ValentinB <valentin.gabriel.badea@gmail.com>
2022-01-14 22:32:32 +0200

    optimize by using tf to run cosine

--- evaluate-qa.py
+++ evaluate-qa.py
@@ -1,11 +1,12 @@
 from transformers import AutoTokenizer, AutoModel
-from sklearn.metrics.pairwise import cosine_similarity
 import json
+import tensorflow as tf
 
-MODEL = "" # to be filled
-data = json.load(open("constructii-qset-v1.json", encoding="utf-8"))
 
-qids = []`
+MODEL = "bert-base-multilingual-cased"  # to be filled
+data = json.load(open("qa_datasets/construction-qset-v1.json", encoding="utf-8"))
+
+qids = []
 dataset = []
 for item in data:
     if item['qid'] in qids:
@@ -19,29 +20,31 @@ for item in data:
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 model = AutoModel.from_pretrained(MODEL)
-acc = 0
-c = 0
+acc = c = 0
 total = len(dataset)
 for qa_test in dataset:
-    max_cosine = 0
+    max_cosine = qid = 0
     c += 1
-    qid = 0
-    input2 = tokenizer(qa_test['question'], padding="max_length", max_length=128, return_tensors='pt')
-    qav = model(**input2).last_hidden_state.flatten().reshape(1, -1).tolist()
+    tokens = tokenizer(qa_test['question'], padding="max_length", max_length=128, return_tensors='pt')
+    a = tf.constant(model(**tokens).last_hidden_state.reshape(1, -1).tolist())
     for qa_train in [x for ind, x in enumerate(dataset) if
                      dataset[ind]['label'] == qa_test['label'] and dataset[ind]['question'] != qa_test["question"]]:
-        cosine = cosine_similarity(qav, model(**tokenizer(qa_train['question'], padding="max_length", max_length=128,
-                                                          return_tensors='pt')).last_hidden_state.flatten().reshape(1, -1).tolist())
-        if max_cosine < cosine[0]:
-            qid = qa_train['qid']
-            max_cosine = cosine[0]
+        b = tf.constant(model(**tokenizer(qa_train['question'], padding="max_length", max_length=128,
+                                          return_tensors='pt')).last_hidden_state.reshape(1, -1).tolist())
+        similarity = tf.reduce_sum(a[:, tf.newaxis] * b, axis=-1)
+        similarity /= tf.norm(a[:, tf.newaxis], axis=-1) * tf.norm(b, axis=-1)
+
+        cosine = similarity.numpy()
         # given the large number of similar questions in one label, 0.95 cosine would be a great similarity,
         # no need to go through all set
-        if max_cosine > 0.95:
+        if cosine[0] > 0.95:
+            qid = qa_train['qid']
             break
+        elif max_cosine < cosine[0]:
+            qid = qa_train['qid']
+            max_cosine = cosine[0]
     if qid == qa_test['qid']:
         acc += 1
         print(c, acc / c)
 
 print("Accuracy is {}".format(acc / total))
-
